0dbdaa0f43b75433334fa24bc2a9d855fedb5e20
dberling <david.berling@rwth-aachen.de>
2024-02-26 17:08:03 +0100

    add str() around snakemake.input /.output, update intensities

--- scripts/analyze_spatial_resolution.py
+++ scripts/analyze_spatial_resolution.py
@@ -1,8 +1,8 @@
 from neurostim.opt_res_analysis import find_xAPCs_over_light_pwrs
 import pandas as pd
 
-data = pd.read_hdf(snakemake.input)
+data = pd.read_hdf(str(nakemake.input))
 
 result = find_xAPCs_over_light_pwrs(data)
 
-result.to_hdf(snakemake.output, key='first')
+result.to_hdf(str(snakemake.output), key='first')
