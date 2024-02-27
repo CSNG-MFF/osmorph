a9b4b80285f4615e17277fbce421be1301765337
dberling <david.berling@rwth-aachen.de>
2024-02-27 09:35:23 +0100

    Improve spres analusis

--- scripts/analyze_spatial_resolution.py
+++ scripts/analyze_spatial_resolution.py
@@ -1,8 +1,8 @@
-from neurostim.opt_res_analysis import find_xAPCs_over_light_pwrs
+from neurostim.opt_res_analysis import improved_find_xAPC50_over_lps
 import pandas as pd
 
 data = pd.read_hdf(str(snakemake.input))
 
-result = find_xAPCs_over_light_pwrs(data)
+result = improved_find_xAPC50_over_lps(data)
 
 result.to_hdf(str(snakemake.output), key='first')
